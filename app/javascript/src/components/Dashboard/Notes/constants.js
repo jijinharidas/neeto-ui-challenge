7822d697177e3c75efe29ba2ea8d7dd2fd0eb5a8
Jijin K Haridas <jijinkharidas10@gmail.com>
2022-09-13 19:39:20 +0530

    fixes according to review

--- app/javascript/src/components/Dashboard/Notes/constants.js
+++ app/javascript/src/components/Dashboard/Notes/constants.js
@@ -4,7 +4,7 @@ export const NOTES_FORM_INITIAL_FORM_VALUES = {
   title: "",
   description: "",
   role: null,
-  tags: null,
+  tags: [],
 };
 
 export const TAGS = [
@@ -58,13 +58,10 @@ export const NOTES_FORM_VALIDATION_SCHEMA = yup.object().shape({
     .required("Role is required"),
   tags: yup
     .array(
-      yup
-        .object()
-        .nullable()
-        .shape({
-          label: yup.string().oneOf(TAGS.map(tag => tag.label)),
-          value: yup.number().oneOf(TAGS.map(tag => tag.value)),
-        })
+      yup.object().shape({
+        label: yup.string().oneOf(TAGS.map(tag => tag.label)),
+        value: yup.number().oneOf(TAGS.map(tag => tag.value)),
+      })
     )
     .nullable()
     .min(1, "Tag is required")
